030e8bf243e40041f0b7ea3b6ad8bf1b39ada258
JasonPan <jason8399@gmail.com>
2015-10-14 11:10:53 +0800

    add download log.

--- downloader.py
+++ downloader.py
@@ -15,7 +15,10 @@ sel = soup.select('.cistab a')
 for link in sel:
 	res = get(WEBHD + link['href'], cookies=res.cookies, stream=True)
 	filename = res.headers['Content-Disposition'].encode('latin1', 'ignore').decode('big5')[9:]
+	print("Saving " + filename)
 	with open(filename, 'wb') as file:
 		for chunk in res:
 			file.write(chunk)
 		file.close()
+	print("Done!")
+print("Total Download: " + str(len(sel)) + " files.")
